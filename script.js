4c239278710c5be9e490a81e4535c79101737cb6
Feranmi Adeniyi <iadeniyi056@gmail.com>
2026-06-17 18:04:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,10 +22,13 @@ function setClock() {
     secondHand.style.transform = `rotate(${secondDeg}deg)`;
     minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    
+    //digital display
     digital = document.getElementById("digital-time")
     let m = minutes < 10 ? "0" + minutes : minutes
     let s = seconds < 10 ? "0" + seconds : seconds
     digital.innerHTML = hours+":"+m+":"+s
+    // Date display
     let mon = month+1
     
     date.innerHTML= day+"/"+ mon +"/" + year
@@ -36,3 +39,25 @@ function setClock() {
 
     // Run once immediately
     setClock();
+
+//custom theme function    
+function changebg(){
+    const setting = document.getElementById("setting")
+    setting.classList.toggle("hide")
+}
+
+//apply color
+
+function applyColor() {
+    const bgColor = document.getElementById("bgColor").value
+    const hourColor = document.getElementById("hourColor").value
+    const minColor = document.getElementById("minColor").value
+    const secColor = document.getElementById("secColor").value
+    
+    secondHand.style.background = secColor
+    minuteHand.style.background = minColor
+    hourHand.style.background = hourColor
+    body.style.background = bgColor
+    
+    setting.classList.toggle("hide")
+}
